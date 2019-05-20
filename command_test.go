9c9507d74c9fbc2b325d3c1b06f386da1df57a14
autopp <autopp.inc@gmail.com>
2019-05-20 13:17:44 +0900

    refactor: centerize get home directory in test

--- command_test.go
+++ command_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -8,17 +9,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var homeDir string
+
+func TestMain(m *testing.M) {
+	var err error
+	homeDir, err = homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(m.Run())
+}
+
 func TestCfHomeDir(t *testing.T) {
 	c := &Command{
 		Name: "mycommand",
 	}
 
-	home, err := homedir.Dir()
-	if err != nil {
-		panic(err)
-	}
-
-	expected := filepath.Join(home, ".fcfc", "mycommand")
+	expected := filepath.Join(homeDir, ".fcfc", "mycommand")
 	actual, err := c.CfHomeDir()
 	assert.NoError(t, err)
 	assert.Equal(t, actual, expected)
