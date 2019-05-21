33638597d3a2ec83473dbfb92744688ef99884ce
autopp <autopp.inc@gmail.com>
2019-05-21 09:55:53 +0900

    feat: output guard statementes and aliases

--- main.go
+++ main.go
@@ -34,5 +34,33 @@ func run() int {
 	var cfg Config
 	yaml.Unmarshal(b, &cfg)
 
+	fcfcDir := filepath.Join(home, ".fcfc")
+	guardTmpl := `if [ ! -d "%s" ]; then
+  if [ -e "%s" ]; then
+    echo %s is already exists and is not directory >&2
+  else
+    mkdir -p "%s"
+  fi
+fi
+
+`
+	fmt.Printf(guardTmpl, fcfcDir, fcfcDir, fcfcDir, fcfcDir)
+	for _, c := range cfg.Commands {
+		loginAlias, err := c.LoginAlias()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 1
+		}
+		fmt.Println(loginAlias)
+
+		cfAlias, err := c.CfAlias()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			return 1
+		}
+		fmt.Println(cfAlias)
+		fmt.Println()
+	}
+
 	return 0
 }
