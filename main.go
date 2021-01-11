837224c5d909cab66b8cf7da733a7fbb069725dc
autopp <autopp.inc@gmail.com>
2021-01-11 17:16:34 +0900

    chore: update license notation

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2019-2020 Akira Tanimura
+	Copyright (C) 2019-2021 Akira Tanimura
 
 		Licensed under the Apache License, Version 2.0 (the "License");
 		you may not use this file except in compliance with the License.
